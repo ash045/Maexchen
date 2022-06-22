ad7534d53d1087b87126e0af3319fe1734ba597b
sophiebrites <107255647+sophiebrites@users.noreply.github.com>
2022-06-22 12:49:07 +0200

    Update GameView.java

--- src/main/java/com/example/application/views/list/GameView.java
+++ src/main/java/com/example/application/views/list/GameView.java
@@ -4,9 +4,9 @@ import com.example.application.data.entity.Player;
 import com.example.application.data.service.CrmService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
@@ -18,7 +18,18 @@ import com.vaadin.flow.router.Route;
 public class GameView extends HorizontalLayout { 
     Grid<Player> grid = new Grid<>(Player.class); 
     TextField filterText = new TextField();
+    //TextField labelScoreboard = new TextField("Scoreboard");
+    Label labelScoreboard = new Label("Scoreboard");
+    TextField numPlayersField = new TextField();
+    TextField numRoundsField = new TextField();
+    TextField pointLimitField = new TextField();
+    
+    Integer numPlayers = 0;
+    Integer numRounds;
+    Integer pointLimit;
+    
     CrmService service;
+    
 
     public GameView(){
         addClassName("game-view");
@@ -50,10 +61,28 @@ public class GameView extends HorizontalLayout {
         return toolbar;
     }
     
+    //Das macht die Informationen unten, das muss noch angepasst werden, damit eben nicht alles immer angezeigt wird sondenr nur abhängig des GameModes
+    private VerticalLayout getLabel() {
+    	VerticalLayout label = new VerticalLayout();
+    	numPlayersField.setReadOnly(true);
+//    	numPlayers = service.countplayer(); Hier müssen Werte drin sein, sonst gibt es einen Fehler, das sollte man catchen
+    	numPlayersField.setValue(numPlayers + " Players");
+    	
+        numRoundsField.setReadOnly(true);
+        numRoundsField.setValue(numRounds + "Rounds left");
+        
+        pointLimitField.setReadOnly(true);
+        pointLimitField.setValue(pointLimit + "Point limit");
+        
+        label.add(numPlayersField, numRoundsField, pointLimitField);
+    	
+    	return label;
+    }
+    
     //Das macht die gesamte Seitenleiste (Filter+Tabelle)
     private VerticalLayout getScoreboard() {
     	VerticalLayout scoreboard = new VerticalLayout();
-    	scoreboard.add(getToolbar(), getScores());
+    	scoreboard.add(labelScoreboard, getToolbar(), getScores(), getLabel());
     	return scoreboard;
     }
     
