f7dc17a66ab5087d91cad19d2cecfd6768b6a1b3
nsmngs <naseemnegrassus@gmail.com>
2022-07-03 21:31:32 +0200

    Redirect From Landing to Game

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,11 +1,14 @@
 package com.example.application.views.list;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import Response;
 
 @PageTitle("list")
 @Route(value = "")
@@ -21,10 +24,22 @@ public class ListView extends VerticalLayout {
         add(new H2("This place intentionally left emptie"));
         add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
 
+           
+        Button redirect = new Button("Maexchen");
+        redirect.addClickListener(e ->
+            redirect.getUI().ifPresent(ui ->
+                ui.navigate("/game"))
+        );
+
+        add(redirect);
+        
+
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
     }
+
+    
 
 }
